8f33636ecbaecbacf5a111b58bd8c0c789a350e3
Naitik Raj <naitikrajj01@gmail.com>
2026-09-18 03:26:28 +0530

    fix server port for render

--- server.js
+++ server.js
@@ -1,3 +1,42 @@
+// // Create Server
+// const express = require("express");
+// const bodyParser = require("body-parser");
+
+// // Database connection
+// const db = require("./db");
+
+// // Routes
+// const empRoute = require("./Route/empRoute");
+// const studentRoute = require("./Route/studentRoute");
+// require('dotenv').config();
+
+// const app = express();
+// const PORT=process.env ||3000;
+
+// // Body Parser
+// app.use(bodyParser.json());
+
+// // Home Route
+// app.get("/", (req, res) => {
+//   res.send("This is home page");
+// });
+
+// // Employee Routes
+// app.use("/emp", empRoute);
+
+// // Student Routes
+// app.use("/student", studentRoute);
+
+
+// console.log("Mongo URL:", process.env.MONGO_URL);
+// // Start Server
+// // app.listen(PORT, () => {
+// //   console.log("Server is running on port 3000");
+// // });
+// app.listen(process.env.PORT || 3000, () => {
+//     console.log("Server is running");
+// });
+
 // Create Server
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -8,17 +47,19 @@ const db = require("./db");
 // Routes
 const empRoute = require("./Route/empRoute");
 const studentRoute = require("./Route/studentRoute");
-require('dotenv').config();
+
+require("dotenv").config();
 
 const app = express();
-const PORT=process.env ||3000;
+
+const PORT = process.env.PORT || 3000;
 
 // Body Parser
 app.use(bodyParser.json());
 
 // Home Route
 app.get("/", (req, res) => {
-  res.send("This is home page");
+    res.send("This is home page");
 });
 
 // Employee Routes
@@ -27,9 +68,9 @@ app.use("/emp", empRoute);
 // Student Routes
 app.use("/student", studentRoute);
 
-
 console.log("Mongo URL:", process.env.MONGO_URL);
+
 // Start Server
 app.listen(PORT, () => {
-  console.log("Server is running on port 3000");
+    console.log(`Server is running on port ${PORT}`);
 });
